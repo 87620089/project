90e08c427d89e17aa5062927a89ae50384f88598
hj25 <87620089@qq.co>
2017-02-08 11:37:18 +0800

    初始版本

---
+++ NYTimesPro/app/src/main/java/com/example/tl/nytimespro/mvp/view/BaseActivity.java
@@ -0,0 +1,11 @@
+package com.example.tl.nytimespro.mvp.view;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by HJ25 on 2017/2/7.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+
+}
---
+++ NYTimesPro/app/src/main/java/com/example/tl/nytimespro/mvp/view/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.tl.nytimespro.mvp.view;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.example.tl.nytimespro.R;
+
+/**
+ * Created by HJ25 on 2017/2/7.
+ */
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
